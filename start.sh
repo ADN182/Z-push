8858301f607cc5cd543a4428c3d50f930745c06b
ADN182 <adn62@hotmail.com>
2018-10-14 15:57:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 if [ ! -f "/usr/share/z-push/config.php" ]
 then
-    mv /home/z-push-git/config.php /usr/share/z-push/
+    cp /home/z-push-git/config.php /usr/share/z-push/
 fi
 
-if [ ! -f "/usr/share/z-push/config.php" ]
+if [ ! -f "/usr/share/z-push/backend/imap/config.php" ]
 then
-    mv /home/z-push-git/config.php /usr/share/z-push/
+    cp /home/z-push-git/backend/imap/config.php /usr/share/z-push/backend/imap/config.php
+fi
+
+if [ ! -f "/usr/share/z-push/backend/carddav/config.php" ]
+then
+    cp /home/z-push-git/backend/carddav/config.php /usr/share/z-push/backend/carddav/config.php
+fi
+
+if [ ! -f "/usr/share/z-push/backend/caldav/config.php" ]
+then
+    cp /home/z-push-git/backend/caldav/config.php /usr/share/z-push/backend/caldav/config.php
+fi
+
+
+if [ ! -f "/usr/share/z-push/backend/caldav/config.php" ]
+then
+    cp /home/z-push-git/backend/combined/config.php /usr/share/z-push/backend/combined/config.php
 fi
 
 php-fpm

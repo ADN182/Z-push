f5eb259adfb7d2646d3f8d6e619f1c251c7ca5b5
ADN182 <adn62@hotmail.com>
2018-10-14 20:25:40 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
+
+if [ ! -f "/etc/ngnix/z-push.conf" ]
+then
+    cp /home/z-push-git/build/ /etc/ngnix/z-push.conf
+fi
+
 if [ ! -f "/usr/share/z-push/config.php" ]
 then
-    cp /home/z-push-git/config.php /usr/share/z-push/
+    cp /home/z-push-git/src/config.php /usr/share/z-push/
 fi
 
 if [ ! -f "/usr/share/z-push/backend/imap/config.php" ]
 then
-    cp /home/z-push-git/backend/imap/config.php /usr/share/z-push/backend/imap/config.php
+    cp /home/z-push-git/src/backend/imap/config.php /usr/share/z-push/backend/imap/config.php
 fi
 
 if [ ! -f "/usr/share/z-push/backend/carddav/config.php" ]
 then
-    cp /home/z-push-git/backend/carddav/config.php /usr/share/z-push/backend/carddav/config.php
+    cp /home/z-push-git/src/ackend/carddav/config.php /usr/share/z-push/backend/carddav/config.php
 fi
 
 if [ ! -f "/usr/share/z-push/backend/caldav/config.php" ]
 then
-    cp /home/z-push-git/backend/caldav/config.php /usr/share/z-push/backend/caldav/config.php
+    cp /home/z-push-git/src/backend/caldav/config.php /usr/share/z-push/backend/caldav/config.php
 fi
 
 
 if [ ! -f "/usr/share/z-push/backend/caldav/config.php" ]
 then
-    cp /home/z-push-git/backend/combined/config.php /usr/share/z-push/backend/combined/config.php
+    cp /home/z-push-git/src/backend/combined/config.php /usr/share/z-push/backend/combined/config.php
 fi
 
-php-fpm
+php5-fpm
 nginx

7788b48754355647e63224112af0556467b54ca2
ADN182 <adn62@hotmail.com>
2018-10-15 20:56:48 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,35 +1,3 @@
 #!/bin/sh
-
-if [ ! -f "/etc/nginx/conf.d/z-push.conf" ]
-then
-    cp  /home/z-push-git/config/nginx/z-push.conf /etc/nginx/conf.d/z-push.conf
-fi
-
-if [ ! -f "/usr/share/z-push/config.php" ]
-then
-    cp /home/z-push-git/src/config.php /usr/share/z-push/
-fi
-
-if [ ! -f "/usr/share/z-push/backend/imap/config.php" ]
-then
-    cp /home/z-push-git/src/backend/imap/config.php /usr/share/z-push/backend/imap/config.php
-fi
-
-if [ ! -f "/usr/share/z-push/backend/carddav/config.php" ]
-then
-    cp /home/z-push-git/src/ackend/carddav/config.php /usr/share/z-push/backend/carddav/config.php
-fi
-
-if [ ! -f "/usr/share/z-push/backend/caldav/config.php" ]
-then
-    cp /home/z-push-git/src/backend/caldav/config.php /usr/share/z-push/backend/caldav/config.php
-fi
-
-
-if [ ! -f "/usr/share/z-push/backend/caldav/config.php" ]
-then
-    cp /home/z-push-git/src/backend/combined/config.php /usr/share/z-push/backend/combined/config.php
-fi
-
-php-fpm5
+php-fpm7
 nginx
